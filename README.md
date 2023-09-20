66da679cff913259f811017284936df983ee7012
QihangZhang <694499657@qq.com>
2023-09-20 12:44:59 +0800

    Make installable

---
+++ README.md
@@ -0,0 +1,4 @@
+# Seo-Api kits
+## 1. basic info
+This is a package use dataforseo easily.
+https://docs.dataforseo.com/v3/serp/google/organic/overview/?python
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seoapikits',
+    version='0.1_alpha',
+    packages=find_packages(),
+    author="Qihang Zhang, NUS business school",
+    author_email="e0952154@u.nus.edu",
+    description="seo-api kits to search the target in code",
+    long_description=open('README.md').read(),
+    url="https://github.com/qihangZH/seoapikits",
+)
